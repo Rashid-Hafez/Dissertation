0d5442939db1f4e823fe8841f4b1e67a11aef989
Rashid-Hafez <33903491+Rashid-Hafez@users.noreply.github.com>
2019-04-09 17:53:31 +0100

    Omp Version

---
+++ cudas/OmpInc.c
@@ -0,0 +1,82 @@
+#include <stdio.h>      /* printf, NULL */  
+#include <stdlib.h>     /* srand, rand */  
+#include <omp.h>  
+#include <cuda.h>  
+#include <cuda_runtime.h> //for unroll 
+  
+/////////////// MACROS and GLOBALS: //////////////  
+#define N 300000000 
+/////////////////////////////  
+  
+typedef struct{  
+  unsigned long long M;//size  
+  unsigned int p; //partition  
+  unsigned int overflow; //overflow  
+  float * vec;  
+}VECTOR;  
+  
+//////////////////////////////////////////////////////////  
+void CheckI(float *vv, long long s);  
+  
+void randomInit(float *data) 
+{  
+  #pragma unroll 
+    for (int i = 0; i <= N; i++){  
+        data[i] = rand()% (1000 + 1 - 1) + 1;  
+        }  
+}  
+//////////////////////////////////////////////////////////  
+  
+///////////////////////////////////////////////////////////////////////////////////////  
+int main(int argc, char** argv){  
+ 
+  double start_time = omp_get_wtime(); 
+  srand(356);  
+  
+  printf("Initialised\n");  
+  
+  VECTOR v;  
+  v.M = N;  
+  v.p =2;  
+  v.overflow = 0;  
+  
+  float * c;  
+  unsigned long byteSize = (N*sizeof(unsigned long long));  
+ 
+ /////  Malloc Array ////// 
+
+  v.vec = malloc(v.M * sizeof(long long)); 
+  randomInit(v.vec);  
+  
+  printf("Size of vec= %lu \n", byteSize);  
+   
+  double pTime = omp_get_wtime(); 
+  printf("v[1000000] = %f\n", v.vec[1000000]); 
+  c = malloc(v.M * sizeof(long long)); 
+  CheckI(c,v.M); 
+  double time2 = omp_get_wtime()-pTime; 
+  printf("c[1000000] = %f\n", c[1000000]); 
+  free(v.vec); 
+  free(c); 
+  double time1 = omp_get_wtime() - start_time; 
+  printf("Parallel Time = %6f \n",time2); 
+  printf("Whole Time = %6f \n",time1); 
+} 
+ 
+void CheckI(float * vv, long long s){  
+// determine how many elements each process will work on 
+ 
+omp_set_num_threads(32); 
+//determine how many elements each process will work on 
+#pragma omp parallel shared(vv) 
+  { 
+  long long  i, id, numthread = 0; 
+  long long size = s; 
+  id = (long long) omp_get_thread_num(); //gets the current thread number 
+  numthread = (long long) omp_get_num_threads(); 
+  for(i = id; i<=size; i+=numthread)  
+    {    
+        vv[i]*=3.3; 
+    } 
+ } 
+} 
